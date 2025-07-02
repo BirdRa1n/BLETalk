36c01076393e02ab164adf0bb06985bfaaaf6f7d
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2025-07-02 10:07:08 -0300

    Add pull-to-refresh to devices list
    Introduced a RefreshControl to the devices list screen, allowing users to refresh the list of Bluetooth devices by pulling down. The refresh indicator adapts to the current color scheme and triggers a new scan for devices.

--- app/(tabs)/(devices)/index.tsx
+++ app/(tabs)/(devices)/index.tsx
@@ -1,15 +1,16 @@
-import { View, Text, FlatList, TouchableOpacity, ScrollView } from 'react-native';;
+import { View, Text, FlatList, TouchableOpacity, ScrollView, RefreshControl, useColorScheme } from 'react-native';
 import { Link, router } from 'expo-router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useBle } from '~/contexts/BleContext';
 import { usePermissions } from '~/hooks/usePermissions';
 import List from '~/components/swift-ui/list';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-
 export default function DevicesScreen() {
     const { devices, isScanning, startScan } = useBle();
     const permissionsGranted = usePermissions();
+    const [refreshing, setRefreshing] = useState(false);
+    const colorScheme = useColorScheme();
 
     useEffect(() => {
         if (permissionsGranted) {
@@ -17,11 +18,32 @@ export default function DevicesScreen() {
         }
     }, [permissionsGranted, startScan]);
 
+    const onRefresh = async () => {
+        setRefreshing(true);
+        try {
+            await new Promise(resolve => setTimeout(resolve, 1000)); // Aguarda 1 segundo
+            await startScan(); // Inicia a busca por novos dispositivos
+        } finally {
+            setRefreshing(false); // Finaliza o estado de refresh
+        }
+    };
+
     return (
         <ScrollView
             className="flex h-full"
             contentInsetAdjustmentBehavior="automatic"
             showsVerticalScrollIndicator={false}
+            refreshControl={
+                <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                    tintColor={colorScheme === 'dark' ? '#fff' : '#000'}
+                    title="Atualizando lista de dispositivos"
+                    titleColor={colorScheme === 'dark' ? '#fff' : '#000'}
+                    progressViewOffset={-15}
+                    style={{ transform: [{ scaleX: 0.2 }, { scaleY: 0.2 }] }}
+                />
+            }
         >
             <View className="flex-1 p-5 gap-4">
                 {permissionsGranted ? (
@@ -32,11 +54,18 @@ export default function DevicesScreen() {
                             items={devices.map(device => ({
                                 label: device.name ?? device?.id,
                                 type: 'navigation',
-                                iconLeft: <Ionicons name="bluetooth" size={20} color="white" className='bg-blue-500 p-2 rounded-md' />,
+                                iconLeft: (
+                                    <Ionicons
+                                        name="bluetooth"
+                                        size={20}
+                                        color="white"
+                                        className="bg-blue-500 p-2 rounded-md"
+                                    />
+                                ),
                                 onPress: () => router.push(`/(tabs)/(devices)/${device.id}`)
                             }))}
                             title="Found Devices"
-                            description='Click on a device to view its information'
+                            description="Click on a device to view its information"
                         />
                     </>
                 ) : (
